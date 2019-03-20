ea7449e19df8a4dfb8fd0c8a4cd430bd5c7ea755
thiagohernandes <hernandes_sp@yahoo.com.br>
2019-03-20 13:35:24 -0300

    Configurações do Service Gateway Zuul

--- gateway-service/src/main/java/com/invillia/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/invillia/gatewayservice/GatewayServiceApplication.java
@@ -1,13 +1,15 @@
 package com.invillia.gatewayservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
+		new SpringApplicationBuilder(GatewayServiceApplication.class).run(args);
 	}
 
 }
