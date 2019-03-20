8caa6040554d65082cfea562c5f649267991ee5b
thiagohernandes <hernandes_sp@yahoo.com.br>
2019-03-20 13:20:54 -0300

    Configuração inicial do projeto

---
+++ businness-service/src/main/java/com/invillia/businnessservice/BusinnessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.invillia.businnessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinnessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinnessServiceApplication.class, args);
+	}
+
+}
